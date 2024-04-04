6d165e0286b612048adabb2ee5cdb77a130b0bea
chenyulin <chenyulin@sjtu.edu.cn>
2024-04-04 14:12:03 +0800

    feat. multiple menu

---
+++ Assets/Script/UI/DetailMenuChooser.cs
@@ -0,0 +1,24 @@
+using Microsoft.MixedReality.Toolkit;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetailMenuChooser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ChooseMenu(string name)
+    {
+        string wholeName = name + " Menu";
+        foreach (Transform child in transform)
+        {
+            if (child.name == wholeName)
+            {
+                child.gameObject.SetActive(true);
+            }
+            else
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+}
