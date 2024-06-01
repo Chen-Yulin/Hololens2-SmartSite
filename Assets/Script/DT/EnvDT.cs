92e8d693ad5bcd661214fce553621bec7739861c
chenyulin <chenyulin@sjtu.edu.cn>
2024-06-01 16:00:25 +0800

    init. Digital Twin

---
+++ Assets/Script/DT/EnvDT.cs
@@ -0,0 +1,30 @@
+using Microsoft.MixedReality.Toolkit;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvDT : MonoBehaviour
+{
+    public bool Initialized = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Initialized)
+        {
+            try
+            {
+                Transform dt = GameObject.Find("MixedRealityPlayspace").transform.Find("Spatial Awareness System");
+                dt.SetParent(transform);
+                Initialized = true;
+            }
+            catch { }
+            
+        }
+    }
+}
